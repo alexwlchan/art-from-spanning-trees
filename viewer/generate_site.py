6df34463d734fac63fc9db5e1c046e212ac7cac5
Alex Chan <alex@alexwlchan.net>
2022-08-11 20:08:58 +0100

    toss in a bunch of images

--- viewer/generate_site.py
+++ viewer/generate_site.py
@@ -1,18 +1,43 @@
 #!/usr/bin/env python3
 
+import datetime
 import os
+from xml.etree import ElementTree as ET
 
 
 def find_svg_paths():
     for name in os.listdir("images"):
         if name.endswith(".svg"):
-            yield os.path.join("images", name)
+            path = os.path.join("images", name)
+
+            # filter out invalid SVG
+            try:
+                root = ET.parse(path).getroot()
+            except ET.ParseError as err:
+                continue
+
+            yield path
+
+
+def get_time(p):
+    if 'Aug 2022' in p:
+        timestamp = p.split()[4]
+        hour, minute, second = timestamp.split(':')
+
+        return datetime.datetime(
+            2022, 7, 10, int(hour), int(minute), int(second)
+        ).timestamp()
+    else:
+        return os.stat(p).st_mtime
 
 
 if __name__ == '__main__':
+    for p in find_svg_paths():
+        print(p, get_time(p))
+
     svg_paths = sorted(
         find_svg_paths(),
-        key=lambda p: os.stat(p).st_mtime,
+        key=lambda p: get_time(p),
         reverse=True
     )
 
@@ -20,13 +45,13 @@ if __name__ == '__main__':
 
     lines.append('''
     <style>
-    
+
     body {
         max-width: 800px;
         margin-left: auto;
         margin-right: auto;
     }
-    
+
     img {
         width: 150px;
         height: 150px;
