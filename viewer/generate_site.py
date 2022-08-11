17ef7f11678efee151b083f8cc73bb5500d5fef8
Alex Chan <alex@alexwlchan.net>
2022-08-11 20:29:46 +0100

    toss in some captions

--- viewer/generate_site.py
+++ viewer/generate_site.py
@@ -5,6 +5,30 @@ import os
 from xml.etree import ElementTree as ET
 
 
+CAPTIONS = {
+    "circle_Wed 10 Aug 2022 12:54:14 BST.svg": {
+        "date": datetime.date(2022, 8, 10),
+        "text": "i tried to make some spider’s web-like graphs, but with straight edges instead of curves. these start to look quite interesting, but i was still working out the kinks &ndash; these graphs aren’t always connected, and sometimes there are loops"
+    },
+    "circle_Wed 10 Aug 2022 07:11:33 BST.svg": {
+        "date": datetime.date(2022, 8, 10),
+        "text": "i tried to make a sort of spider's web-like graph, with concentric rings and spokes coming out from the centre. i didn’t really know how to draw arcs properly, so the curves aren’t in the right place &ndash; a lot of these have loops or the lines don’t meet each other properly. i liked the idea, but shelved the curves for a bit"
+    },
+    "tringle_Wed 10 Aug 2022 06:33:36 BST.svg": {
+        "date": datetime.date(2022, 8, 10),
+        "text": "i discovered a function called <code><a href=\"https://networkx.org/documentation/stable/reference/generated/networkx.generators.lattice.triangular_lattice_graph.html?highlight=triangular+lattice#networkx.generators.lattice.triangular_lattice_graph\">triangular_lattice_graph</a></code> that dropped straight into my code and made some slightly different pictures. i’m still not really sure what it’s meant to be used for but i think these graphs look nice"
+    },
+    "Wed 10 Aug 2022 06:23:31 BST.svg": {
+        "date": datetime.date(2022, 8, 10),
+        "text": "my earliest experiments, drawing graphs on a simple 2D square lattice"
+    },
+    "Wed 10 Aug 2022 06:17:26 BST.svg": {
+        "date": datetime.date(2022, 8, 10),
+        "text": "a very early sketch. i must have mucked up the coordinates"
+    }
+}
+
+
 def find_svg_paths():
     for name in os.listdir("images"):
         if name.endswith(".svg"):
@@ -44,12 +68,27 @@ if __name__ == '__main__':
     lines = ['<html>']
 
     lines.append('''
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <title>spanning tree art</title>
+    </head>
+    ''')
+
+    lines.append('''
     <style>
 
+    p {
+        max-width: 750px;
+    }
+
     body {
-        max-width: 800px;
+        max-width: 950px;
         margin-left: auto;
         margin-right: auto;
+        padding: 1em;
+        line-height: 1.25em;
     }
 
     img {
@@ -61,7 +100,26 @@ if __name__ == '__main__':
 
     lines.append('<h1>spanning tree art</h1>')
 
+    lines.append('''
+    <p>this is some procedurally generated art using <a href="https://en.wikipedia.org/wiki/Minimum_spanning_tree">maximum spanning trees</a>.</p>
+
+    <p>how it works:</p>
+    <ol>
+    <li>create a <a href="https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)">graph</a> &ndash; in the mathematical sense, a network of nodes and edges</li>
+    <li>assign random weights to each edge</li>
+    <li>find a maximally spanning tree of the weighted graph. visit every point, use as many edges as possible, but never have a loop</li>
+    </ol>
+    <p>this is a gallery of some of the pictures i&rsquo;ve made this way. i think they&rsquo;re pretty.</p>
+
+    <p>made with &lt;3 by <a href="https://alexwlchan.net">alexwlchan</a>. code on <a href="https://github.com/alexwlchan/art-from-spanning-trees">github</a></p>
+
+    <hr/>
+    ''')
+
     for p in svg_paths:
+        if os.path.basename(p) in CAPTIONS:
+            caption = CAPTIONS[os.path.basename(p)]
+            lines.append(f"<p><strong>{caption['date'].strftime('%d %B %Y').lower()} / </strong> {caption['text']}</p>")
         lines.append(f'<a href="/{p}"><img src="/{p}"></a>')
 
     lines.append('</html>')
